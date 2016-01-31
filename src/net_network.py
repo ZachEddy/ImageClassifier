1dd919bb1621a7ead09cd81f78ac66fed9c62846
ZachEddy <zeddy@pugetsound.edu>
2016-01-31 15:03:38 -0800

    loaded cifar-10 as image volumes

--- src/net_network.py
+++ src/net_network.py
@@ -1,14 +1,17 @@
 # a class made to manage feeding images to the network
 from src.layers.conv_layer import conv_layer
 from src.layers.pool_layer import pool_layer
+import load_cifar
 
 class net_network:
 
-	layers = []
-
 	def __init__(self, layer_structure):
+		self.layers = []
 		self.build_layers(layer_structure)
-
+		# load the cifar-10 images and their corresponding labels
+		cifar_data = load_cifar.images_to_volumes()
+		self.image_volumes = cifar_data[0]
+		self.image_labels = cifar_data[1]
 
 	def build_layers(self, layer_structure):
 		# build a layer based on what is provided in the 'initialization' module
