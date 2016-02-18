e2c917f47679c7a7a488cec7d33e16f200c2d8c4
ZachEddy <zeddy@pugetsound.edu>
2016-02-17 22:45:31 -0800

    changed volume's constructor to take a single 3D ndarray

--- src/volume.py
+++ src/volume.py
@@ -1,20 +1,10 @@
 # a class to hold volumes of numerical values
 # this could also be described as a 3D array 
-
 import numpy as np
 class volume:	
-	# def __init__(self, width, height):
-	# 	self.width = width
-	# 	self.height = height
-	# 	self.volume_slices = []
-
 	def __init__(self, volume):
+		# initialize height, width, and depth according to the 3D array entered by the user
 		self.width = len(volume[0][0])
 		self.height = len(volume[0])
+		self.depth = len(volume)
 		self.volume_slices = volume
-
-	def add_volume_slice(self, *args):
-		for volume_slice in enumerate(args):
-			# access [1] element because enumerate() puts *args into a tuple, where the other element is the index.
-			# yeah... took me forever to figure that one out.
-			self.volume_slices.append(volume_slice[1])
