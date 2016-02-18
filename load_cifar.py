f8837909efa7451e94c298cf6461b2cf61ba1539
ZachEddy <zeddy@pugetsound.edu>
2016-02-17 18:46:00 -0800

    using numpy correctly to load CIFAR-10 images

--- load_cifar.py
+++ load_cifar.py
@@ -21,14 +21,7 @@ def images_to_volumes():
 	# first 1024 are red, next 1024 are green, final 1024 are blue. Pretty awesome!
 	# each array of 3072 is put into an array of 10,000 (because 10,000 total images)
 	for rgb_array in batch_data['data']:
-		# add each rgb channel as its own layer (depth_slice) to the volume
-		r_slice = np.reshape(rgb_array[0:1024], (32,32))
-		g_slice = np.reshape(rgb_array[1024:2048], (32,32))
-		b_slice = np.reshape(rgb_array[2048:3072], (32,32))
-		image_volume = volume(32,32)
-		image_volume.add_volume_slice(r_slice, g_slice, b_slice)
-		# add to list containing all image volumes
+		image_volume = volume(np.reshape(rgb_array, (3,32,32)))
 		volumes.append(image_volume)
-
-
+		
 	return (volumes, batch_data['labels'])
