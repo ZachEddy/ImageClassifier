4f93a3b4ce2ef5f941b8090418109783118886bc
ZachEddy <zeddy@pugetsound.edu>
2016-02-18 17:17:29 -0800

    input layer now normalizes pixel values correctly

--- src/layers/input_layer.py
+++ src/layers/input_layer.py
@@ -1,4 +1,5 @@
 import numpy as np
+from src.volume import volume
 # input layer should be in charge of modifying pixel values (0-255)
 # this will include normalization and mean value subtraction
 class input_layer:
@@ -7,17 +8,14 @@ class input_layer:
 		self.out_height = out_height
 		self.out_width = out_width
 		self.out_depth = out_depth
-
-	
-	def forward(self, input_volume):
-		# this takes an input and normalizes the pixel values (0-255) to be something from -0.5 to 0.5
-		for volume_slice in input_volume.volume_slices:
-			volume_slice = self.normalize_matrix(volume_slice)
-
-	def normalize_pixel(pixel_value):
+		# vectorize the pixel normalization function so it can easily be mapped over matrices
+		self.normalize_volume = np.vectorize(self.normalize_pixel)
+ 
+	# a function to noramlize a single pixel values
+	def normalize_pixel(self, pixel_value):
 		# normalize pixel values between 0.5 and -0.5
 		return pixel_value/255.0 - 0.5
 
-	# vectorizing the normalize_pixel function allows me to easy map it over an entire matrix
-	# rather than using a nested for loop (it's the same process under the hood, though)
-	normalize_matrix = np.vectorize(normalize_pixel)
+	# the feed-forward function for an input layer - it's extremely simple
+	def forward(self, input_volume):
+		return volume(self.normalize_volume(input_volume.volume_slices))
