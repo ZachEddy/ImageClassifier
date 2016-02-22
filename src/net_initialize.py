a2cd1a3bab83e97027cd3a9fa92c73f12f7e6db2
ZachEddy <zeddy@pugetsound.edu>
2016-02-21 21:30:08 -0800

    added a more realistic layer pattern

--- src/net_initialize.py
+++ src/net_initialize.py
@@ -7,9 +7,26 @@ layer_structure = []
 
 # continue to add layers (ReLu, conv, pool, softmax, etc)
 layer_structure.append({'type':'input', 'out_height':32, 'out_width':32, 'out_depth':3})
-layer_structure.append({'type':'conv', 'field_size':3, 'filter_count':20, 'stride':1, 'padding':0})
+
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':16, 'stride':1, 'padding':2})
 layer_structure.append({'type':'relu'})
+
 layer_structure.append({'type':'pool', 'field_size':2})
+
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':20, 'stride':1, 'padding':2})
+layer_structure.append({'type':'relu'})
+
+layer_structure.append({'type':'pool', 'field_size':2})
+
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':20, 'stride':1, 'padding':2})
+layer_structure.append({'type':'relu'})
+
+layer_structure.append({'type':'pool', 'field_size':2})
+
+
+
+# layer_structure.append({'type':'fully_connected', 'neuron_count':100})
+
 
 # create a network with the layer specificications
 net = net_network(layer_structure)
