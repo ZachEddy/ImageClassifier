e547abb74fc0cf6e817de1f16610f45e8c0afe6c
ZachEddy <zeddy@pugetsound.edu>
2016-02-23 20:45:11 -0800

    added the forward function to fully connected layers

--- src/layers/fc_layer.py
+++ src/layers/fc_layer.py
@@ -1,7 +1,7 @@
 import numpy as np
 from src.volume import volume
 # a class to manage the ReLu activation layer
-class relu_layer: 
+class fc_layer: 
 	def __init__(self, in_height, in_width, in_depth, neuron_count):
 		# define input dimensions
 		self.in_height = in_height
@@ -9,17 +9,31 @@ class relu_layer:
 		self.in_depth = in_depth
 
 		# define output dimensions
-		self.out_height = neuron_count
-		self.out_width = 1
+		self.out_height = 1
+		self.out_width = neuron_count
 		self.out_depth = 1
 
-		def initalize_weights(self):
-			weights = []
-			weight_count = self.in_height * self.in_width * self.in_depth
-			for i in self.neuron_count:
-				weight_gen = np.random.randn(weight_count) * np.sqrt(2.0 / weight_count)
-				weights.append(weight_gen)
+		# setup the weights
+		self.initalize_weights()
 
-		def forward(self, input_volume):
-			# I don't do anything yet
-			return
+	# a function to initialize the weights of the fully connected layer
+	def initalize_weights(self):
+		weights = []
+		weight_count = self.in_height * self.in_width * self.in_depth
+		# initialize weights for each neuron in the network
+		for i in range(self.out_width):
+			weight_gen = np.random.randn(weight_count) * np.sqrt(2.0 / weight_count)
+			weight_gen = np.reshape(weight_gen, (self.in_depth, self.in_height, self.in_width))
+			weights.append(weight_gen)
+		# set as an instance variable for later methods
+		self.weights = weights
+
+	# a function that feeds an input volume through the network
+	def forward(self, input_volume):
+		output = np.array([])
+		# each matrix of weights corresponds to a slice in the output volume
+		for weight in self.weights:
+			output = np.append(output, np.sum(weight * input_volume.volume_slices) + 0.1)
+		
+		# maybe redefine which axix the wights are on (height, width, or depth)?
+		return volume(np.reshape(output, (1,1, self.out_width)))
