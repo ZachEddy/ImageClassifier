55b16ebd059dc1113df981654645c1d78fd2f8b2
ZachEddy <zeddy@pugetsound.edu>
2016-02-23 20:48:22 -0800

    added a layer pattern that includes fc and tanh layers

--- src/net_initialize.py
+++ src/net_initialize.py
@@ -23,10 +23,12 @@ layer_structure.append({'type':'relu'})
 
 layer_structure.append({'type':'pool', 'field_size':2})
 
+layer_structure.append({'type':'fully_connected', 'neuron_count':100})
+layer_structure.append({'type':'relu'})
 
+layer_structure.append({'type':'fully_connected', 'neuron_count':10})
 
-# layer_structure.append({'type':'fully_connected', 'neuron_count':100})
-
+layer_structure.append({'type':'tanh'})
 
 # create a network with the layer specificications
 net = net_network(layer_structure)
