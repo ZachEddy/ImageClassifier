d4430bd497c70d3ca9faed4858d1db340a18cc4c
ZachEddy <zeddy@pugetsound.edu>
2016-03-20 18:18:01 -0700

    softmax now computes gradients

--- src/layers/softmax_layer.py
+++ src/layers/softmax_layer.py
@@ -15,11 +15,34 @@ class softmax_layer:
 
 	# a function that feeds the input volume through the network
 	def forward(self, input_volume):
-		# map e^x across the input volume
-		exp_matrix = np.exp(input_volume.volume_slices)
+		# subtract the max output from all output values to avoid exponential explosion
+		max_input = np.max(input_volume.volume_slices)
+		exp_matrix = input_volume.volume_slices - max_input
 
-		# normalize the matrix values - they now represent individual probabilities
-		exp_matrix_sum = np.sum(exp_matrix) 
+		# map e^x across list to find unnormalized probabilities. save them for backpropagation
+		exp_matrix = np.exp(exp_matrix)
+		# self.exp = exp_matrix
+
+		# normalize the probabilities to be between 0 and 1
+		exp_matrix_sum = np.sum(exp_matrix)
 		exp_matrix = exp_matrix / exp_matrix_sum
+
+		# save volumes for backprop
+		self.input_volume = input_volume
+		self.output_volume = volume(exp_matrix)
+		return self.output_volume
+
+	# a function to find gradients of the softmax layer
+	def backward(self, label):
+		# zero out existing gradients
+		self.input_volume.zero_gradient()
+		for i in range(len(self.output_volume.volume_slices[0,0,:])):
+			# check if output corresponds probability to the actual label (only something we know during training)
+			# if an output doesn't match the label, it will be zero
+			label_hit = 1 if i == label else 0
+
+			# find gradient by subtracting the probability from the label hit (either one or zero)
+			self.input_volume.gradient_slices[0,0,i] = -(label_hit - self.output_volume.volume_slices[0,0,i])
 		
-		return volume(exp_matrix)
+		# evaluate the loss and store it as an instance variable
+		self.loss = -np.log10(self.output_volume.volume_slices[0,0,label])
