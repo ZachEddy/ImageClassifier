620d7719305dc8596cdb041ce26bae0244dbd72e
ZachEddy <zeddy@pugetsound.edu>
2016-03-29 17:28:44 -0700

    added ugly code to finally test network

--- src/net_network.py
+++ src/net_network.py
@@ -7,19 +7,51 @@ from src.layers.input_layer import input_layer
 from src.layers.relu_layer import relu_layer
 from src.layers.fc_layer import fc_layer
 from src.layers.tanh_layer import tanh_layer
+from src.layers.softmax_layer import softmax_layer
 from src.volume import volume
+import time
+
 
 class net_network:
 	def __init__(self, layer_structure):
 		# load the cifar-10 images and their corresponding labels
-		cifar_data = load_cifar.images_to_volumes()
+		cifar_data = load_cifar.images_to_volumes("image_data/data_batch_1")
+		test_data = load_cifar.images_to_volumes("image_data/data_batch_2")
 		self.image_volumes = cifar_data[0]
 		self.image_labels = cifar_data[1]
+
+		test_volumes = test_data[0]
+		test_labels = test_data[1]
 
 		# generate the layers based off of layer definitions from user
 		self.layers = []
 		self.build_layers(layer_structure)
-		self.forward(self.image_volumes[0])
+		start = time.time()
+
+		# print self.image_labels
+		for i in range(len(self.image_volumes)):
+			print i, "train"
+			label = self.image_labels[i]
+			self.forward(self.image_volumes[i])
+			self.backward(label)
+			self.train(0.01)
+			# print self.layers[len(self.layers)-1].classify
+
+		end = time.time()
+		print end - start
+
+		correct = 0.0
+		test_amount = 1000
+		for i in range(test_amount):
+			self.forward(test_volumes[i])
+			result = self.layers[len(self.layers)-1].classify
+			print i, "test"
+			if result == test_labels[i]:
+				correct = correct + 1
+		print (correct / test_amount) * 100
+		
+		
+		# self.forward(self.image_volumes[0])
 
 	# a function that initializes each layer depending on the user specs.
 	# basically a massive if/elif/else block
@@ -85,6 +117,13 @@ class net_network:
 				self.layers.append(new_layer)
 				previous = new_layer
 				# add the new conv layer (dupe code)
+			elif layer['type'] == 'softmax':
+				new_layer = softmax_layer(
+					previous.out_height,
+					previous.out_width,
+					previous.out_depth)
+				self.layers.append(new_layer)
+				previous = new_layer
 			elif layer['type'] == 'fully_connected':
 				# fully connected layer
 				new_layer = fc_layer(
@@ -99,169 +138,205 @@ class net_network:
 				quit()
 		# print self.layers
 
-	# input volume should be an image volume
 	def forward(self, image_volume):
-
-		print "START!"
-		# print image_volume.volume_slices
-		# print "_--__-_---------~~~~~~~~~~~~~~"
+		# go through each layer and compute the forward pass
+		
 		input_volume = self.layers[0].forward(image_volume)
-		for layer in self.layers:
-			if type(layer) == "input_layer":			
-				continue
-
-			# print input_volume.volume_slices
-			# print layer, "_______________________________________________________________"
-			# print ""
+		for layer in self.layers[1:]:
 			input_volume = layer.forward(input_volume)
-
-		print "DONE!"
-
-		print input_volume.volume_slices
-
-		# vol = np.array([[[1,2],[3,4]],[[4,5],[6,7]]])
-		# print "vol", vol
-		# # def __init__(self, in_height, in_width, in_depth, neuron_count):
-		# fc = fc_layer(2,2,2,2)
-		# vol = volume(vol)
-
-		# vol = fc.forward(vol)
-
-		# print "fc", vol.volume_slices
-
-		# tanh = tanh_layer(1,1,2)
-
-		# vol = tanh.forward(vol)
-		# print "tanh", vol.volume_slices
-		# print image_volume.volume_slices
-		# first_vol = self.layers[0].forward(image_volume)
-
-		# # print first_vol.volume_slices
-
-		# second_vol = self.layers[1].forward(first_vol)
-		# # print second_vol.volume_slices
-		# # ignores the relu layer
-		# third_vol = self.layers[3].forward(second_vol)
-		# print third_vol.volume_slices
+		np.sum(input_volume.volume_slices)
 
 
+	def backward(self, label):
+		flip_list = self.layers[::-1]
+
+		output_with_gradients = flip_list[0].backward(label)
+		for layer in flip_list[1:len(flip_list)-1]:
+			layer.output_volume = output_with_gradients
+			output_with_gradients = layer.backward()
+
+	def train(self, rate):
+		for layer in self.layers:
+			layer.train(rate)
+	# input volume should be an image volume
+	def test(self, image_volume):
+		
+
+		input_volume = [
+		[[1,2,2,1,2],
+		[1,1,1,0,0],
+		[0,0,0,2,0],
+		[2,0,0,0,1],
+		[0,2,2,2,1]],
+		
+		[[2,2,0,2,2],
+		[1,2,0,2,2],
+		[1,1,0,1,0],
+		[2,0,1,0,2],
+		[1,2,0,2,2]],
+		
+		[[2,2,1,0,2],
+		[0,0,2,2,0],
+		[1,0,1,2,1],
+		[2,1,1,2,2],
+		[0,0,2,2,1]]
+		]
+		input_volume = volume(np.array(input_volume) * 1.0)
+
+		filter_one = [
+		[[0,1,1],
+		[1,0,1],
+		[0,1,-1]],
+		[[1,0,-1],
+		[0,0,-1],
+		[1,-1,-1]],
+		[[0,1,0],
+		[1,1,0],
+		[-1,0,0]]
+		]
+		filter_one = volume(np.array(filter_one) * 1.0)
+
+		filter_two = [
+		[[-1,1,-1],
+		[0,0,1],
+		[-1,0,-1]],
+		[[-1,1,1],
+		[0,-1,0],
+		[-1,-1,-1]],
+		[[1,0,0],
+		[-1,0,-1],
+		[-1,1,1]]
+		]
+		filter_two = volume(np.array(filter_two) * 1.0)
+
+		# def __init__(self, field_size, filter_count, stride, padding, in_height, in_width, in_depth):
+		conv = conv_layer(3,2,2,1,5,5,3)
+		filters = [filter_one, filter_two]
+		conv.filters = filters
+		conv.biases = np.array([1,0])
+
+		output_volume = conv.forward(input_volume)
+		print output_volume.volume_slices
+		gradients = np.reshape(range(18), (2,3,3)) * 1.0
+		gradients.fill(1)
+		output_volume.gradient_slices = gradients
 
 
-		# # testing the pool layer
-		# test_volume = random.sample(range(-100,100), 192)
-		# test_volume = np.reshape(test_volume, (3,8,8))
-		# test_volume = volume(test_volume)
+			
+		conv.backward()
+
+		print "Start"
+		for f in conv.filters:
+			print "volume and grads:"
+			print f.gradient_slices
+			print f.gradient_slices
+		print "filter grads\n"
+		print conv.input_volume.volume_slices, "inputs\n"
+		print conv.input_volume.gradient_slices, "grad\n"
 
 
 
-		# print test_volume.volume_slices
-		# relu_test = relu_layer(3,8,8)
-		# test_vol = relu_test.forward(test_volume)
-		# print "-----"
-		# print test_vol.volume_slices
-
-
+		# print output_volume.volume_slices
+		# print output_volume.gradient_slices
 
 		
-		# print self.layers[1].forward(input_volume)
+		# print filter_one
+		# # relu
+		# # create the layer
+		# relu = relu_layer(2,2,3)
 
-
-
-
-
-		# Tons of commented code that I don't want to delete. I'm sure it'll never be useful again... but who knows!?
-
-		# # print data
-
-		# # print f 
-		# # quit()
-		# # data[256, 256] = [255, 0, 0]
-		# # img = Image.fromarray(data, 'RGB')
-		# # img.save('my.png')
-		# # 
-		# rgb = []
-		# for i in range(input_volume.height):
-		# 	f = []
-		# 	for j in range(input_volume.width):
-		# 		f.append([input_volume.volume_slices[0][i][j], input_volume.volume_slices[1][i][j], input_volume.volume_slices[2][i][j]])
-		# 	rgb.append(f)
-
-
-
-		# # # print len(input_volume.volume_slices[0][0])
-		# # rgb = np.array(input_volume.volume_slices, dtype=np.uint8)
-		# img = Image.fromarray(np.array(rgb), 'RGB')
-		# # print data
-
-		# # img = Image.fromarray(rgb, 'RGB')
-		# img.save('my.png')
-
-		# # # self.layers[1].forward(input_volume)
-
-
-
-
-		# t = self.layers[1]
-		# t.in_height = 7
-		# t.in_width = 7
-		# t.in_depth = 3
-		# t.field_size = 3
-		# t.stride = 2
-		# t.filter_count = 2
-		# t.calc_output_dimensions()
-
-		# print t.out_depth, t.out_width, t.out_height
-
-
-		# input_vol = [0,0,0,0,0,0,0,0,0,1,0,2,1,0,0,2,2,1,1,0,0,0,2,2,0,1,0,0,0,0,0,0,1,0,0,0,1,1,2,1,0,0,0,0,0,0,0,0,0,
-		# 	0,0,0,0,0,0,0,0,2,2,1,1,1,0,0,2,0,0,2,1,0,0,1,2,1,2,0,0,0,1,1,0,0,0,0,0,1,0,0,2,0,0,0,0,0,0,0,0,0,
-		# 	0,0,0,0,0,0,0,0,2,0,2,0,2,0,0,0,1,1,0,0,0,0,2,1,0,0,2,0,0,1,0,2,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
-
-		# input_vol = np.reshape(input_vol, (3,7,7))
+		# # make the input volume
+		# input_volume = np.array(range(-6,6))
+		# input_volume = np.reshape(input_volume, (3,2,2))
+		# input_volume = volume(input_volume)
 		
+		# # print the input volume
+		# print input_volume.volume_slices, "1: input\n"
 		
-		# # print input_vol[0]
-		# for i in range(len(input_vol)):
-		# 	input_vol[i] = np.transpose(input_vol[i])
+		# # make the output volume
+		# output_volume = relu.forward(input_volume)
+		# print output_volume.volume_slices, "2: output\n"
 
-		# # print input_vol
+		# # make the output gradients, assign them to the output
+		# output_gradients = np.array(range(-6,6)[::-1])
+		# output_gradients = np.reshape(output_gradients, (3,2,2))
 
-		# # f_1 = [1,0,0,1,0,-1,0,-1,-1,0,-1,-1,-1,1,0,1,0,-1,-1,-1,1,-1,1,1,1,-1,-1]
-		# # f_2 = [1,1,0,1,1,-1,-1,0,-1,1,0,0,1,0,1,1,1,-1,0,1,1,-1,0,0,0,1,-1]
+		# output_volume.gradient_slices = output_gradients
+		# print output_gradients, "3: output grad\n"
+
+		# relu.backward()
+
+		# print relu.input_volume.gradient_slices, "4: input grad\n"
+		# # print input_volume.gradient_slices
+		# # relu
+
+
+
+		# input_volume = np.array([2,3,4,5,1,2,3,4,3,6,8,4,3,6,7,0,5,3,6,3,7,2,1,7,9,6,0,5,7,4,3,5,6,7,5,8,0,6,3,4,2,5,7,0,2,10,5,7])
+		# input_volume = np.reshape(input_volume, (3,4,4))
+		# input_volume = volume(input_volume)
+		# print input_volume.volume_slices
+
+		# pool = pool_layer(2,4,4,3)
+		# output_volume = pool.forward(input_volume)
+		# pool.output_volume.gradient_slices.fill(1)
+
+
+		# pool.backward()
+		# print input_volume.gradient_slices
+		# print output_volume.volume_slices
+		# print "______________________________________________________ COL"
+		# print pool.max_col_positions
+		# print "______________________________________________________ ROW"
+		# print pool.max_row_positions
+
+
+
+		# fully con
+
+		# input_volume = np.array([2,3,4,5,1,2,3,4])
+		# input_volume = volume(np.reshape(input_volume, (2,2,2)))
+
+		# weight_one = np.array([1,2,3,4,5,6,7,8])
+		# weight_one = volume(np.reshape(weight_one, (2,2,2)))
+
+		# weight_two = np.array([8,7,6,5,4,3,2,1])
+		# weight_two = volume(np.reshape(weight_two, (2,2,2)))
+
+		# weight_list = [weight_one, weight_two]
+		# fully = fc_layer(2,2,2,2)
+		# fully.weights = weight_list
+		# fully.biases[0][0][1] = 2
+
+		# print fully.forward(input_volume).volume_slices
+
+		# fully.backward()
+		# fully conn 
+	
+		# # here, bro
+		# neurons = np.array([3.2, 5.1, -1.7])
+
+		# neurons = np.reshape(neurons, (1,1,3))
+		# print neurons
+		# vol = volume(neurons)
+
+		# softmax = softmax_layer(1,1,10)
+
+		# softmax.forward(vol)
+		# softmax.backward(0)
+
+		# print softmax.input_volume.gradient_slices
+
+		# return
+		# # end here
+
+
+
+		# input_volume = self.layers[0].forward(image_volume)
+		# for layer in self.layers:
+		# 	if type(layer) == "input_layer":			
+		# 		continue
 		
-		# # f_2 = [1,1,-1,1,1,0,0,-1,-1,1,1,1,0,0,1,0,1,-1,0,-1,0,1,0,1,1,0,-1]
-		# f_1 = np.reshape(f_1,(3,3,3))
-		# f_2 = np.reshape(f_2,(3,3,3))
+		# 	input_volume = layer.forward(input_volume)
 
-		# # f_1 = np.transpose(f_1)
-		# # f_2 = np.transpose(f_2)
-
-		# f_1 = volume(f_1)
-		# f_2 = volume(f_2)
-
-		# f = [f_1, f_2]
-		# print f_1.volume_slices
-
-		# t.filters = f
-		
-		# print t.forward(volume(input_vol))
-
-
-		# # self.field_size = field_size
-		# # self.filter_count = filter_count
-		# # self.stride = stride
-		# # self.padding = padding
-
-		# # # specify input dimensions
-		# # self.in_height = in_height
-		# # self.in_width = in_width
-		# # self.in_depth = in_depth
-
-		# # print input_volume.volume_slices[:,0,0]
-		# # quit()
-		# # self.layers[1].forward(input_volume)
-
-		# return 
-		# # self.layers[1].forward(np.array(input_volume))
-		# # current_volume = input_volume
-		# # for layer in self.layers:
+		# print "Probabilites:", input_volume.volume_slices
