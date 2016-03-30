b5cfacf27d740f63fb08f22655ae028b6dcc7fc3
ZachEddy <zeddy@pugetsound.edu>
2016-03-29 17:27:35 -0700

    added some code to enable training

--- src/layers/softmax_layer.py
+++ src/layers/softmax_layer.py
@@ -30,6 +30,9 @@ class softmax_layer:
 		# save volumes for backprop
 		self.input_volume = input_volume
 		self.output_volume = volume(exp_matrix)
+
+		self.classify = np.argmax(self.output_volume.volume_slices)
+		# print self.output_volume.volume_slices
 		return self.output_volume
 
 	# a function to find gradients of the softmax layer
@@ -40,10 +43,13 @@ class softmax_layer:
 			# check if output corresponds probability to the actual label (only something we know during training)
 			# if an output doesn't match the label, it will be zero
 			label_hit = 1 if i == label else 0
-			
+
 			# find gradient by subtracting the probability from the label hit (either one or zero)
 			self.input_volume.gradient_slices[0,0,i] = -(label_hit - self.output_volume.volume_slices[0,0,i])
 		
 		# evaluate the loss and store it as an instance variable
 		self.loss = -np.log10(self.output_volume.volume_slices[0,0,label])
 		return self.input_volume
+
+	def train(self, rate):
+		return
