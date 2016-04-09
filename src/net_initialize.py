c7585c047024311dcbe9ac6655ab592f088b97be
ZachEddy <zeddy@pugetsound.edu>
2016-04-09 14:16:59 -0700

    changed layer pattern and added a network trainer

--- src/net_initialize.py
+++ src/net_initialize.py
@@ -1,6 +1,7 @@
 # define the organization of layers in the network
 # to make it flexible to changes in structure
 from src.net_network import net_network
+from src.net_trainer import net_trainer
 
 # ordered structure of layers in the neural network
 layer_structure = []
@@ -13,12 +14,12 @@ layer_structure.append({'type':'relu'})
 
 layer_structure.append({'type':'pool', 'field_size':2})
 
-layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':20, 'stride':1, 'padding':2})
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':16, 'stride':1, 'padding':2})
 layer_structure.append({'type':'relu'})
 
 layer_structure.append({'type':'pool', 'field_size':2})
 
-layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':20, 'stride':1, 'padding':2})
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':16, 'stride':1, 'padding':2})
 layer_structure.append({'type':'relu'})
 
 layer_structure.append({'type':'pool', 'field_size':2})
@@ -32,3 +33,4 @@ layer_structure.append({'type':'softmax'})
 
 # create a network with the layer specificications
 net = net_network(layer_structure)
+net_trainer(net, learning_rate = 0.001)
