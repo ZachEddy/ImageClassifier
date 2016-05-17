1052608b2b31dd3bd9fd81b68b0751d86c25bf39
ZachEddy <zeddy@pugetsound.edu>
2016-05-17 11:37:50 -0700

    explained how to initialize or load networks in comments

--- src/net_initialize.py
+++ src/net_initialize.py
@@ -8,17 +8,17 @@ layer_structure = []
 # continue to add layers (ReLu, conv, pool, softmax, etc)
 layer_structure.append({'type':'input', 'out_height':32, 'out_width':32, 'out_depth':3})
 
-layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':16, 'stride':1, 'padding':2, 'name':"conv_one"})
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':10, 'stride':1, 'padding':2, 'name':"conv_one"})
 layer_structure.append({'type':'relu'})
 
 layer_structure.append({'type':'pool', 'field_size':2})
 
-layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':16, 'stride':1, 'padding':2, 'name':"conv_two"})
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':10, 'stride':1, 'padding':2, 'name':"conv_two"})
 layer_structure.append({'type':'relu'})
 
 layer_structure.append({'type':'pool', 'field_size':2})
 
-layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':16, 'stride':1, 'padding':2, 'name':"conv_three"})
+layer_structure.append({'type':'conv', 'field_size':5, 'filter_count':10, 'stride':1, 'padding':2, 'name':"conv_three"})
 layer_structure.append({'type':'relu'})
 
 layer_structure.append({'type':'pool', 'field_size':2})
@@ -32,4 +32,12 @@ layer_structure.append({'type':'softmax'})
 
 # generate a new neural network with the given specs
 def build():
-	return net_network(None, "test_network")
+	# generate untrained network; give the network a name
+	# return net_network(layer_structure,"new_network_name")
+
+	# alternatively, load an existing network with 'None' as the layer specs
+	# and 'saved_network_name' as the name of the network.
+	# 'sgd_one' is the name of the trained network that I used for the demo (50.4% accuracy on
+	# 1000 images from the testing set)
+	
+	return net_network(layer_structure,"sgd_one")
